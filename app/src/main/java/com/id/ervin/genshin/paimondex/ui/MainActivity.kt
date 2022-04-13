b1a225926621d801912ec63c5b5e5f0f6ac03f96
ervinsu <ervin.suriandi@linecorp.com>
2022-04-13 18:51:15 +0700

    :fire: Remove unused fab

--- app/src/main/java/com/id/ervin/genshin/paimondex/ui/MainActivity.kt
+++ app/src/main/java/com/id/ervin/genshin/paimondex/ui/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.id.ervin.genshin.paimondex.R
 import com.id.ervin.genshin.paimondex.databinding.ActivityMainBinding
 
@@ -27,10 +26,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
