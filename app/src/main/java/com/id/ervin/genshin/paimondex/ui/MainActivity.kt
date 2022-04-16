1ee8b91c30421355f36d63a84cb9740d2bd87109
ervinsu <ervin.suriandi@linecorp.com>
2022-04-16 21:35:19 +0700

    :lipstick: Create About page fragment

--- app/src/main/java/com/id/ervin/genshin/paimondex/ui/MainActivity.kt
+++ app/src/main/java/com/id/ervin/genshin/paimondex/ui/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_characters, R.id.nav_favorite, R.id.nav_slideshow
+                R.id.nav_characters, R.id.nav_favorite, R.id.nav_about
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/id/ervin/genshin/paimondex/ui/feature/about/AboutViewModel.kt
+++ app/src/main/java/com/id/ervin/genshin/paimondex/ui/feature/about/AboutViewModel.kt
@@ -1,13 +1,13 @@
-package com.id.ervin.genshin.paimondex.ui.feature.slideshow
+package com.id.ervin.genshin.paimondex.ui.feature.about
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is About Fragment"
     }
     val text: LiveData<String> = _text
 }
